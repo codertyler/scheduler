0de4493df8933bd6987f26ea2c6a946c4038db8d
codertylerasai@gmail.com <codertylerasai@gmail.com>
2020-10-28 09:28:42 -0600

    initial commit

---
+++ src/components/InterviewerList.js
@@ -0,0 +1,33 @@
+import React from "react";
+import PropTypes from "prop-types";
+import InterviewerListItem from "./InterviewerListItem";
+import './InterviewerList.scss';
+
+export default function InterviewerList(props) {
+  const { interviewers, interviewer, setInterviewer } = props;
+
+  const interviewersGroup = interviewers.map(interviewerTeacher => {
+    return (
+      <InterviewerListItem
+        key = {interviewerTeacher.id}
+        name = {interviewerTeacher.name}
+        avatar = {interviewerTeacher.avatar}
+        selected = {interviewerTeacher.id == interviewer}
+        setInterviewer={event => setInterviewer(interviewerTeacher.id)}
+        />
+    )
+  })
+
+  return (
+    <section className="interviewers">
+      <h4 className="interviewers__header text--light">Interviewer</h4>
+      <ul className="interviewers__list"></ul>
+      {interviewersGroup}
+    </section>
+  );
+}
+
+InterviewerList.propTypes = {
+  interviewers: PropTypes.array.isRequired
+};
+
---
+++ src/components/InterviewerListItem.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./InterviewerListItem.scss";
+import classNames from "classnames";
+
+
+export default function InterviewerListItem(props) {
+  const { name, selected, avatar, setInterviewer } = props;
+
+  const interviewersClass = classNames("interviewers__item", {
+    "interviewers__item--selected": selected,
+  });
+
+  return (
+    <li className={interviewersClass} onClick={setInterviewer}>
+      <img className="interviewers__item-image" 
+            src={avatar} 
+            alt={name} />
+      {selected ? name : null}
+    </li>
+  );
+}
