a0e21e5eb3356d25abf3d74aa688dc0316ec9e69
codertylerasai@gmail.com <codertylerasai@gmail.com>
2020-11-03 12:56:26 -0700

    added first real test for applicatio test js

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,11 +1,17 @@
 import React from "react";
+import axios from "axios";
 
-import { render, cleanup } from "@testing-library/react";
+import { render, cleanup, waitForElement, fireEvent } from "@testing-library/react";
 
 import Application from "components/Application";
 
 afterEach(cleanup);
 
-it("should render without crashing", () => {
-  render(<Application />);
-});
+it("defaults to Monday and changes the schedule when a new day is selected", async () => {
+  const { getByText } = render(<Application />);
+
+    await waitForElement(() => getByText("Monday")).then(() => {
+      fireEvent.click(getByText("Tuesday"));
+      expect(getByText("Leopold Silvers")).toBeInTheDocument();
+    });
+})
